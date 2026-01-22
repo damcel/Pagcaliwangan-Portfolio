104b1677c0b5e93cd86793e20a2cdd8276a8dbff
Daniel Pagcaliwangan <pagcaliwangan11@gmail.com>
2026-01-22 13:43:09 +0800

    portfolio update/certificate download and view as pdf

--- script/certificate-modal.js
+++ script/certificate-modal.js
@@ -2,11 +2,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     const certificateCards = document.querySelectorAll('.certificate-card');
     const modalImage = document.querySelector('#fullCertificateModal .modal-full-cert');
+    const viewPdfBtn = document.getElementById('viewPdfBtn');
+    const downloadPdfBtn = document.getElementById('downloadPdfBtn');
 
     certificateCards.forEach(card => {
         card.addEventListener('click', () => {
             const fullSrc = card.getAttribute('data-full');
+            const pdfSrc = card.getAttribute('data-pdf');
+
             modalImage.setAttribute('src', fullSrc);
+
+            // Set PDF links
+            viewPdfBtn.setAttribute('href', pdfSrc);
+            downloadPdfBtn.setAttribute('href', pdfSrc);
         });
     });
 });
